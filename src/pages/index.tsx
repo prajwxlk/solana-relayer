9a2793d465db9debe64046c1d36acb517956a9f8
Prajwal <prajwify@gmail.com>
2022-09-08 13:58:12 +0530

    dummy date data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -277,10 +277,10 @@ const Home: NextPage = () => {
                                   {keys}
                                 </th>
                                 <td className="py-4 px-6 font-medium text-gray-100 whitespace-nowrap">
-                                  18/08/2022
+                                  DD/MM/YYYY
                                 </td>
                                 <td className="py-4 px-6 font-medium text-gray-100 whitespace-nowrap">
-                                  28/12/2022
+                                  DD/MM/YYYY
                                 </td>
                                 <td className="py-4 px-6">
                                   <button type="button" className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
